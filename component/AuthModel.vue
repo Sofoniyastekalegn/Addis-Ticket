7d42275bcaaff7cc47a1b9577bd1510c2e6b04e1
Sofoniyastekalegn <sofoniyastekalegn@gmail.com>
2025-08-29 11:06:32 -0700

    moviecard quickbook

---
+++ component/AuthModel.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+const message = ref("Nuxt")
+
+const hello = () => sayHello(message.value)
+
+
+</script>
+
+<template>
+    <main>
+        <h1>Demo with auto imports</h1>
+
+        <form @submit.prevent="hello">
+            <MyInput v-model="message" />
+            <button type="submit">Say Hello</button>
+        </form>
+    </main>
+</template>
